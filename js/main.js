9d9fc801390fbac0ff640e7d526638e91c096d09
yahyaoui-mohamed <hamayah4@gmail.com>
2020-10-13 18:16:48 +0100

    edit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-let nav = document.querySelector(".nav");
-let navItems = document.querySelectorAll(".nav li");
-let links = document.querySelectorAll(".nav li a");
-let slide = document.querySelector("span.slide i");
-let scrollTop = document.querySelector(".top");
-let settingBox = document.querySelector(".setting-box");
+let nav         = document.querySelector(".nav");
+let navItems    = document.querySelectorAll(".nav li");
+let links       = document.querySelectorAll(".nav li a");
+let slide       = document.querySelector("span.slide i");
+let scrollTop   = document.querySelector(".top");
+let settingBox  = document.querySelector(".setting-box");
+let settingIcon = document.querySelector(".config");
+let closeBtn    = document.querySelector(".close");
+let navbar      = document.querySelector(".navbar");
+
 navItems.forEach(item => {
   item.onclick = function()
   {
@@ -14,6 +18,7 @@ navItems.forEach(item => {
   }
 });
 
+
 links.forEach(link => {
   link.onclick = function(e)
   {
@@ -21,42 +26,56 @@ links.forEach(link => {
   }
 });
 
+
 slide.onclick = function(){
   window.scrollTo(0,window.innerHeight);
 }
+
 
 scrollTop.onclick = function(e){
   e.preventDefault();
   window.scrollTo(0,0);
 }
 
+
 this.onscroll = function()
 {
   if( this.pageYOffset >= 400 )
   { 
-    nav.classList.add("animate__fadeInDown","fixed");
+    navbar.classList.add("animate__fadeInDown","fixed");
     scrollTop.style.opacity = "1";
     scrollTop.style.visibility = "visible";
   }
   else{
-    nav.classList.remove("animate__fadeInDown","fixed");
+    navbar.classList.remove("animate__fadeInDown","fixed");
     scrollTop.style.opacity = "0";
     scrollTop.style.visibility = "hidden";
 
   }
 }
 
-settingBox.onmouseover = function()
+
+
+settingIcon.onmouseover = function()
 {
-  this.style.left = 0;
+  this.style.left = "-32px";
+  setTimeout(function(){
+
+    settingBox.style.left = 0;
+
+  },600)
 }
 
-settingBox.onmouseleave = function(){
-  let box = this;
+
+
+closeBtn.onclick = function(){
+  settingBox.style.left = "-45px";
+
   setTimeout(function(){
-    box.style.left = "-45px";
-  },500);
+    settingIcon.style.left = 0;
+  },800);
 }
+
 
 settingBox.querySelectorAll("ul li").forEach(item => {
   item.onclick = function()
